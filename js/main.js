de8823794bc0b1777ef67a8b722f937faa01747e
Kenan862 <82305454+Kenan862@users.noreply.github.com>
2022-05-29 10:33:38 +0400

    Battery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { battery } from "./components/battery.js";
 import { clock } from "./components/clock.js";
 import { desktop_context_menu } from "./components/desktop_context_menu.js";
 import { full_screen } from "./components/full_screen.js";
@@ -6,6 +7,7 @@ import { taskbar_buttons } from "./components/taskbar_buttons.js";
 $(document).ready(() => {
   taskbar_buttons();
   clock();
+  battery();
   $(document).one("click", "body", full_screen);
     $(document).contextmenu(()=>{
         desktop_context_menu();
